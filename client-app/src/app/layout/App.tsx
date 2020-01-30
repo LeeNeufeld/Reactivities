48843bd4262f00ea93caf7c7f193f0f881d9834d
Lee Neufeld <leeneufeld23@hotmail.com>
2020-01-30 09:56:19 -0700

    End Section 55

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { Header, Icon, List } from "semantic-ui-react";
+import axios from "axios";
+import { IActivity } from "../models/activity";
+import { Navbar } from "../../features/nav/NavBar";
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then(response => {
+        setActivities(response.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <List>
+        {activities.map(activity => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export default App;
